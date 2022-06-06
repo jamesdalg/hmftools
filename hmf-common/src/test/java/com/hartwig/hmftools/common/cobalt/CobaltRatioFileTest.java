51953c00614679c97955c0ccc6a4f1b973b3b06e
Dalgleish <dalgleishjl@nih.gov>
2022-06-06 15:49:52 -0400

    cobalt work

--- hmf-common/src/test/java/com/hartwig/hmftools/common/cobalt/CobaltRatioFileTest.java
+++ hmf-common/src/test/java/com/hartwig/hmftools/common/cobalt/CobaltRatioFileTest.java
@@ -15,11 +15,16 @@ public class CobaltRatioFileTest {
 
     private static final String V37_PATH = Resources.getResource("cobalt/cobalt.37.tsv").getPath();
     private static final String V38_PATH = Resources.getResource("cobalt/cobalt.38.tsv").getPath();
-
+    private static final String CHM13_PATH = Resources.getResource("cobalt/cobalt.CHM13.tsv").getPath();
     @Test
     public void testV38() throws IOException {
         final List<CobaltRatio> v38 = Lists.newArrayList(CobaltRatioFile.read(V38_PATH).get(HumanChromosome._1));
         assertEquals(5, v38.size());
+    }
+    @Test
+    public void testCHM13() throws IOException {
+        final List<CobaltRatio> vCHM13 = Lists.newArrayList(CobaltRatioFile.read(CHM13_PATH).get(HumanChromosome._1));
+        assertEquals(5, vCHM13.size());
     }
 
     @Test
